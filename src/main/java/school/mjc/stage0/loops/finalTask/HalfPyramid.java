eaceeb83c37d62279b69423e0597228734e324b0
BekoSm <b.smagulov.1999@gmail.com>
2022-07-27 17:54:19 +0600

    Push#1

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,10 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
-    }
-
-    public static void main(String[] args) {
-        new HalfPyramid().printHalfPyramid(8);
+        int i,j,k;
+        for(i=cathetusLength;i>=1;i--){
+            for(j=i;j>=2;j--){
+                System.out.print(" ");
+            }
+            for(k=1;k<=cathetusLength-i+1;k++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,17 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
-    }
-
-    public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        for (int i=1; i<=cathetusLength; i++){//outer forloop
+            for (int j=1; j<=(cathetusLength-i); j++){
+                System.out.print(" ");//create initial space for pyramid shape
+            }
+            for (int k=i; k>=1; k--){//inner for loops
+                System.out.print(""+k);//create left half
+            }
+            for (int l=2; l<=i; l++){
+                System.out.print(""+l);    //create right half
+            }//end outer for loop
+            System.out.println();
+        }
     }
 }
